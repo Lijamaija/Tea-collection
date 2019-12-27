780117dd161bc31b50f7d2080f0770988cf4286b
Lijamaija <liza.timofeeva@gmail.com>
2019-12-27 22:29:31 +0100

    Updates

---
+++ precache-manifest.19a6b2bad59e2834626a76cfd06c65c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd9ca14887576c0d4574068ad048f86",
+    "url": "/Tea-collection/index.html"
+  },
+  {
+    "revision": "79e7cfa782603651cdc8",
+    "url": "/Tea-collection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "79e7cfa782603651cdc8",
+    "url": "/Tea-collection/static/js/2.b8e34c0e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tea-collection/static/js/2.b8e34c0e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3835cb7c98ed95e5d4e7",
+    "url": "/Tea-collection/static/js/main.4a7495d6.chunk.js"
+  },
+  {
+    "revision": "661232f4e36706113bce",
+    "url": "/Tea-collection/static/js/runtime-main.21cd173b.js"
+  }
+]);
