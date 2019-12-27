fbf53c632185fdabe8efc81da85cd6b606142424
Lijamaija <liza.timofeeva@gmail.com>
2019-12-27 22:01:58 +0100

    'practice'

---
+++ src/Containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import { robots } from '../robots';
+import Scroll from '../Components/Scroll';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        //what changes in app
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+   
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    
+    }
+    render() {
+        //destructuring 
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robots => {
+        return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        if (!robots.length) {
+            return <h1>Loading</h1>
+        } else {
+    return (
+        <div className='tc'>
+            <h1>Robofriends</h1>
+            <SearchBox searchChange={this.onSearchChange} />
+            <Scroll> <CardList robots={filteredRobots} />
+                </Scroll>
+        </div>
+    );
+}
+}
+}
+
+export default App;
